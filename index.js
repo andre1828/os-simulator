7688e091cbcbba199a636246878e2332053c7aef
Andre Melo <andre.melo@softtek.com>
2020-04-10 04:49:31 -0300

    Implementar modal de configurações iniciais

--- index.js
+++ index.js
@@ -3,8 +3,29 @@ import { EventBus } from "./EventBus.js"
 let v = new Vue({
   el: "#app",
   data: {
-    message: "bla",
+    showModal: true,
+    processTable: [
+      { id: 0, totalTime: 20 },
+      { id: 1, totalTime: 20 },
+      { id: 2, totalTime: 20 },
+    ],
+    runningProcesses: [
+      { id: 0, remainingTime: 20 },
+      { id: 1, remainingTime: 20 },
+      { id: 2, remainingTime: 20 },
+    ],
+    readyProcesses: [
+      { id: 0, totalTime: 20 },
+      { id: 1, totalTime: 20 },
+      { id: 2, totalTime: 20 },
+    ],
   },
+  methods: {
+    startSimulation: function() {
+      v.$data.showModal = false
+      console.log("BOOYA")
+    }
+  }
 })
 
 function onEvent(eventData) {
