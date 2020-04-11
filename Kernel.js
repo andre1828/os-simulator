edd74f804447728792c3545da2c4f0a2effa1d02
Andre Melo <andre.melo@softtek.com>
2020-04-11 03:25:54 -0300

    run and createProcess implementation in Kernel

--- Kernel.js
+++ Kernel.js
@@ -1,16 +1,26 @@
 import Scheduler from "./Scheduler.js"
+import Process from "./Process.js"
 export default class Kernel {
-  constructor(numOfCores) {
+  constructor(numOfCores, scheduler) {
     this.processTable = []
     this.cpu = new Array(numOfCores)
     this.processId = 0
+    this.scheduler = new Scheduler(numOfCores)
   }
 
   run(numOfInitialProcesses) {
     console.log("kernel run")
-    // instantiate scheduler
+    // create initial processes
+    for (let i = 0; i < numOfInitialProcesses; i++) {
+      this.createProcess(++this.processId, Math.floor(Math.random() * 21))
+    }
   }
-  createProcess() {}
+  createProcess(id, totalTime) {
+    var newProcess = new Process(id, totalTime)
+    this.processTable.push(newProcess)
+    // give process to scheduler
+    this.scheduler.insertProcess(newProcess)
+  }
   killProcess() {}
   runProcess() {}
 }
