2c1024bebb14892429a255bb76d0cf4bc8374b37
Andre Melo <andre.melo@softtek.com>
2020-04-12 16:09:00 -0300

    start clock after 3 seconds

--- Clock.js
+++ Clock.js
@@ -1,5 +1,7 @@
 onmessage = function () {
-  setInterval(() => {
-    this.postMessage("tick")
-  }, 1000)
+  setTimeout(() => {
+    setInterval(() => {
+      this.postMessage("tick")
+    }, 1000)
+  }, 3000)
 }
